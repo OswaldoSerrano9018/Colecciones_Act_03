5f0893f33781297e5450467dec51a56f9ec38eaa
OswaldoSerrano9018 <46874727+OswaldoSerrano9018@users.noreply.github.com>
2023-09-30 14:25:50 -0600

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card {
+    private String palo;
+    private String color;
+    private String valor;
+
+    public Card(String Palo, String Color, String Valor) {
+        palo = Palo;
+        color = Color;
+        valor = Valor;
+    }
+
+    public void RenglonBorrar() {
+        System.out.println(palo + ", " + color + ", " + valor);
+    }
+    public String PaloGet() { return palo; }
+    public String ColorGet() { return color; }
+    public String ValorGet() { return valor; }
+}
